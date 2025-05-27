8eb49ac422eaa36c167040bf0af5d0e576a2ea78
ChristosGkotzai <christosgoxhaj@gmail.com>
2025-05-27 17:11:08 +0000

    areaCircle

---
+++ areaCircle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+const float pi=3.1415926;
+
+int main(){
+	float a,radius;
+	cout<<"Give the radius:"<<endl;
+	cin >> radius;
+	a=radius*radius*pi;
+	cout << "the area is"<<a << endl;
+	return 0;
+	
+}
