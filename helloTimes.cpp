39dec34f5f441a87f2a0f0c24c57c3bf8fdd9d8d
ChristosGkotzai <christosgoxhaj@gmail.com>
2025-05-27 16:57:52 +0000

    updated with negative check

--- helloTimes.cpp
+++ helloTimes.cpp
@@ -7,8 +7,13 @@ int main(){
 	int num;
 	cout <<"Give a num to print the hello"<<endl;
 	cin >>num;
+	if (num <0){
+		cout << "negative" << endl;	
+	}
+	else{
 	for(int i=0;i<num;i++){
 		hello();		
+	}
 	}
 	return 0;
 }
